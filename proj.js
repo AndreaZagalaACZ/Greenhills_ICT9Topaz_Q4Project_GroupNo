f93861b153ee88e3fa38f08c65fbf51502221b0c
Andrea Zagala <zagala.ac@obmontessori.edu.ph>
2026-05-04 21:27:12 +0800

    Update proj.js

--- proj.js
+++ proj.js
@@ -1,31 +1,30 @@
 // JavaScript Login Authentication
+let loginAttempts = 0;
+const maxAttempts = 3;
+
 function loginAuthentication() {
-
-    let loginAttempts = 0;
-    const maxAttempts = 3;
-
     const username = document.getElementById('input').value;
     const password = document.getElementById('input2').value;
 
+    if (loginAttempts >= maxAttempts) {
+        alert('You have used all 3 tries. Refresh the page to try again.');
+        return;
+    }
+
     for (let i = 0; i < 1; i++) {
-    loginAttempts++;
+        loginAttempts++;
 
-    if (username === 'DonQuiLC' && password === 'HeroMili') {
-      window.alert('Welcome back!');
-      window.location.replace('dashboard.html');
-      return;
-    }
+        if (username === 'DonQuiLC' && password === 'HeroMili') {
+            window.alert('Welcome back!');
+            window.location.replace('dashboard.html');
+            return;
+        }
 
-    if (loginAttempts < maxAttempts) {
-      window.alert('Incorrect Username or Password. Try again ' + loginAttempts + '/' + maxAttempts + '.');
-    } else {
-      window.alert('Incorrect Username or Password. No more tries left.');
-      document.getElementById('loginBtn').disabled = true;
-    }
-    }
-
-   if (loginAttempts >= maxAttempts) {
-    alert('You have used all 3 tries. Refresh the page to try again.');
-    return;
+        if (loginAttempts < maxAttempts) {
+            window.alert('Incorrect Username or Password. Try again ' + loginAttempts + '/' + maxAttempts + '.');
+        } else {
+            window.alert('Incorrect Username or Password. No more tries left.');
+            document.getElementById('loginBtn').disabled = true;
+        }
     }
 }
